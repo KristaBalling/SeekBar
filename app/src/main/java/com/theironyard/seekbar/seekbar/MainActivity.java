294fd7de35e1717cb2e4b530633ad63977073ec1
Krista Blachian <KristaBlachian@outlook.com>
2018-06-20 16:25:45 -0400

    initial commit

---
+++ app/src/main/java/com/theironyard/seekbar/seekbar/MainActivity.java
@@ -0,0 +1,42 @@
+package com.theironyard.seekbar.seekbar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private SeekBar seekBar;
+    private TextView resultTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultTextView = (TextView) findViewById(R.id.resultID);
+        seekBar = (SeekBar) findViewById(R.id.seekBarID);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+               // Log.d("SB", "OnProgress!");
+                resultTextView.setText("Pain Level: " + seekBar.getProgress() + "/" + seekBar.getMax());
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Log.d("SB", "OnStartTrackingTouch!!");
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Log.d("SB", "OnStopTrackingTouch!!");
+
+            }
+        });
+
+    }
+}
